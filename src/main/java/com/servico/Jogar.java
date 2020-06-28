2f7e64a1cf73956e6ff5b3499498838911ac3ae6
Daniel Marmitt <danielmarmitt@hotmail.com>
2020-06-28 00:24:34 -0300

    API Consumo

---
+++ src/main/java/com/servico/Jogar.java
@@ -0,0 +1,14 @@
+package com.servico;
+
+/**
+ * 
+ * @author Daniel Marmitt
+ * RestController, expondo API para possibilitar jogadas 
+ */
+public class Jogar {
+	
+	public void iniciarJogo(int jogador, String tipoJogada) {
+		
+	}
+
+}
