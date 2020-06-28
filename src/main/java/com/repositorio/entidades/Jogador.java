abdaa755df546698083c293e51948a054cf49aed
Daniel Marmitt <danielmarmitt@hotmail.com>
2020-06-27 23:55:00 -0300

    Levantando entidades

---
+++ src/main/java/com/repositorio/entidades/Jogador.java
@@ -0,0 +1,38 @@
+package com.repositorio.entidades;
+
+import java.io.Serializable;
+
+/*
+ * @author daniel.marmitt
+ */
+public class Jogador implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nome;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "id:"+this.nome + ";Nome:"+this.nome;
+	}
+	
+	@Override
+	public boolean equals(Object arg0) {
+		return super.equals(arg0);
+	}
+}
---
+++ src/main/java/com/repositorio/entidades/TipoJogada.java
@@ -0,0 +1,28 @@
+package com.repositorio.entidades;
+
+/**
+ * 
+ * @author daniel.marmitt
+ * Representa o tipo de jogada que pode ser executada
+ *
+ */
+public enum TipoJogada {
+	PEDRA(1,"Pedra"),PAPEL(2,"Papel"),
+	TESOURA(3,"Tesoura"),LAGARTO(4,"Lagarto"),SPOCK(5,"Spock");
+	
+	private int id;
+	private String descricao;
+	
+
+	TipoJogada(int id, String descricao){
+		this.id = id;
+		this.descricao = descricao;
+		
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+}
